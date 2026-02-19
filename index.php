dd2f00ffdbb0c42edc1f1cb8e626710a7ffaf837
Carmine Pistone <carmine.pistone.apostoli@gmail.com>
2026-02-19 20:11:46 +0100

    selfwork 6 completato

--- index.php
+++ index.php
@@ -20,19 +20,42 @@
 //     }
 // }
 
-$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-$counter = 0;
-$total = 0;
+// Selfwork 5
 
-foreach($numbers as $number){
-    if($number %2 == 0){
-        $total += $number;
-        $counter++; 
+
+// $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// $counter = 0;
+// $total = 0;
+
+// foreach($numbers as $number){
+//     if($number %2 == 0){
+//         $total += $number;
+//         $counter++; 
+//     }
+// }
+
+
+// echo $total/$counter;
+
+
+for ($i = 1; $i <= 100; $i++) {
+    switch($i){
+        case $i % 15 == 0:
+            echo "HACKADEMY \n";
+            break;
+        case $i % 3 == 0:
+            echo "PHP \n";
+            break;
+        case $i % 5 == 0:
+            echo "JAVASCRIPT \n";
+            break;
+
+        default:
+            echo "$i \n";
+            break;
     }
 }
-
-
-echo $total/$counter;
 
 
